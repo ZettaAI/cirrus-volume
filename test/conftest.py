1318b8141202d380fdb60400f38510217eed5ded
NT <nturner@zetta.ai>
2022-02-24 15:04:26 -0500

    test: rules.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,21 +1,30 @@
-'''
+"""
 Testing setup and teardown functionality
 
 Creates a dummy CloudVolume and tears it down if specified below
 (by commenting/uncommenting... I know)
-'''
+"""
 import os
 import glob
+
 import pytest
 import cloudvolume as cv
+import cirrusvolume as crv
+import provenancetoolbox as ptb
 
 
-# Location of the dummy CloudVolume
+# dummy CloudVolume information
 DUMMY_CV_PATH = "./test/test_cv"
+SOURCES = ['tracer annotation']
+MOTIVATION = 'proper documentation is helpful'
+PROCESS = ptb.Process(
+              'testing CirrusVolume',
+              {'patience for humor': 6/10},
+              ptb.PythonGithubEnv('.'))
 
 
 def make_testcloudvolume(path):
-    'Makes a dummy CloudVolume'
+    """Makes a dummy CloudVolume"""
     num_channels = 1
     layer_type = 'segmentation'
     data_type = 'uint32'
@@ -38,39 +47,52 @@ def make_testcloudvolume(path):
 
     
 def removefiles(expr):
-    'Removes all files specified by a glob expression'
+    """Removes all files specified by a glob expression"""
     filenames = glob.glob(expr)
     for filename in filenames:
         os.remove(filename)
 
 
 def teardown(path):
-    'Tears down the creation of a dummy cloudvolume at a particular path'
+    """Tears down the creation of a dummy cloudvolume at a particular path"""
     removefiles(os.path.join(path, "*"))
     os.rmdir(path)
 
 
 @pytest.fixture
 def testcloudvolume_with_td():
-    'Fixture that creates dummy CV and tears it down afterwards'
+    """Fixture that creates dummy CV and tears it down afterwards"""
     yield make_testcloudvolume(DUMMY_CV_PATH)
     teardown(DUMMY_CV_PATH)
 
 
 @pytest.fixture
 def testcloudvolume_no_td():
-    'Fixture that creates dummy CV and leaves it up afterwards'
+    """Fixture that creates dummy CV and leaves it up afterwards"""
     yield make_testcloudvolume(DUMMY_CV_PATH)
 
 
 # Uncomment to skip teardown of the dummy CloudVolume
 # (useful to inspect the resulting files, etc.)
-@pytest.fixture
-def testcloudvolume(testcloudvolume_no_td):
-    return testcloudvolume_no_td
+#@pytest.fixture
+#def testcloudvolume(testcloudvolume_no_td):
+#    return testcloudvolume_no_td
 
 
 # Uncomment to include teardown of the dummy CloudVolume
-#@pytest.fixture
-#def testcloudvolume(testcloudvolume_with_td):
-#    return testcloudvolume_with_td
+@pytest.fixture
+def testcloudvolume(testcloudvolume_with_td):
+    return testcloudvolume_with_td
+
+
+@pytest.fixture
+def readvolume(testcloudvolume):
+    return crv.CloudVolume(testcloudvolume.cloudpath)
+
+
+@pytest.fixture
+def readwritevolume(testcloudvolume):
+    return crv.CloudVolume(
+               testcloudvolume.cloudpath, sources=SOURCES,
+               motivation=MOTIVATION, process=PROCESS
+           )
--- test/test_volume.py
+++ test/test_volume.py
@@ -6,34 +6,12 @@ current installation.
 import pytest
 
 import cirrusvolume as cv
-import provenancetoolbox as ptb
-
-
-SOURCES = ['tracer annotation']
-MOTIVATION = 'proper documentation is helpful'
-PROCESS = ptb.Process(
-              'testing CirrusVolume',
-              {'patience for humor': 6/10},
-              ptb.PythonGithubEnv('.'))
 
 
 def test_creation(testcloudvolume):
     """Testing whether basic instantiation of a CirrusVolume works."""
     cirrusvolume = cv.CloudVolume(testcloudvolume.cloudpath)
     assert type(cirrusvolume) == cv.precomputed.CirrusVolumePrecomputed
-
-
-@pytest.fixture
-def readvolume(testcloudvolume):
-    return cv.CloudVolume(testcloudvolume.cloudpath)
-
-
-@pytest.fixture
-def readwritevolume(testcloudvolume):
-    return cv.CloudVolume(testcloudvolume.cloudpath,
-                          sources=SOURCES,
-                          motivation=MOTIVATION,
-                          process=PROCESS)
 
 
 def test_writeblocking(readvolume):
